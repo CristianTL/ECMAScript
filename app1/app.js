1613410ecbc2517c5aa67b9432ff54017c81b6d7
Thomaz Lourenço <thomazcl1@hotmail.com>
2020-03-31 20:22:41 -0300

    Aplicação de filtro (filter)

--- app1/app.js
+++ app1/app.js
@@ -65,6 +65,45 @@ class Bd{
 
 		return despesas
 	}
+
+	pesquisar(despesa){
+
+		let despesasFiltradas = Array()
+
+		despesasFiltradas = this.recuperarTodosRegistros()
+
+		//ano
+		if(despesa.ano != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+		}
+
+		//mes
+		if(despesa.mes != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+		}
+
+		//dia
+		if(despesa.dia != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+		}
+
+		//tipo
+		if(despesa.tipo != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+		}
+
+		//descricao
+		if(despesa.descricao != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+		}
+
+		//valor
+		if(despesa.valor != ''){
+			despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+		}
+
+		console.log(despesasFiltradas)
+	}
 }
 
 let bd = new Bd()
@@ -165,6 +204,19 @@ function carregaListaDespesas(){
 	})
 
 
+}
+
+function pesquisarDespesa(){
+	let ano = document.getElementById('ano').value
+	let mes = document.getElementById('mes').value
+	let dia = document.getElementById('dia').value
+	let tipo = document.getElementById('tipo').value
+	let descricao = document.getElementById('descricao').value
+	let valor = document.getElementById('valor').value
+
+	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+	bd.pesquisar(despesa)
 }
 
 //function gravar(d){
