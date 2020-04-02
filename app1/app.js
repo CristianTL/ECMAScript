ff5636ebf74799b386862aa6837bbef66727bd78
Thomaz Lourenço <thomazcl1@hotmail.com>
2020-04-01 22:42:48 -0300

    removendo registro e finalizando

--- app1/app.js
+++ app1/app.js
@@ -59,6 +59,7 @@ class Bd{
 				continue
 			}
 
+			despesa.id = i
 			despesas.push(despesa)
 
 		}
@@ -103,6 +104,10 @@ class Bd{
 		}
 
 		return despesasFiltradas
+	}
+
+	remover(id){
+		localStorage.removeItem(id)
 	}
 }
 
@@ -202,6 +207,22 @@ function carregaListaDespesas(despesas = Array(), filtro = false){
 
 		linha.insertCell(2).innerHTML = d.descricao
 		linha.insertCell(3).innerHTML = d.valor
+
+		//criar o botão de exclusão
+		let btn = document.createElement("button")
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class="fas fa-times"></i>'
+		btn.id = 'id_despesa_'+d.id
+		btn.onclick = function(){
+			//remover a despesa
+			let id = this.id.replace("id_despesa_","")
+			
+			bd.remover(id)
+
+			window.location.reload()
+		}
+		linha.insertCell(4).append(btn)
+
 	})
 
 
